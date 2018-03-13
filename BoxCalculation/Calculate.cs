e0df46fc060554a44e89ab3e52d533f8737c05af
lutfiqaraman <lutfiqaraman@gmail.com>
2018-03-13 21:40:00 +0200

    Done

---
+++ BoxCalculation/Calculate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxCalculation
+{
+    public class Calculate
+    {
+        int height;
+        int width;
+        int length;
+
+        public Calculate(int h, int w, int l)
+        {
+            height = h;
+            width  = w;
+            length = l;
+        }
+
+        public int[] Get_size()
+        {
+            int Surface = (2 * length * width) + (2 * length * height) + (2 * width * height);
+            int Volume  = height * length * width;
+
+            int[] values = new int[] { Surface, Volume };
+
+            return values;
+        }
+
+    }
+}
--- BoxCalculation/Program.cs
+++ BoxCalculation/Program.cs
@@ -10,6 +10,15 @@ namespace BoxCalculation
     {
         static void Main(string[] args)
         {
+            Calculate box = new Calculate(5,5,5);
+
+            var Surface = box.Get_size()[0];
+            var Volume  = box.Get_size()[1];
+
+            Console.WriteLine("The surface of box {0}", Surface);
+            Console.WriteLine("The area of box {0}", Volume);
+
+            Console.ReadKey();
         }
     }
 }
